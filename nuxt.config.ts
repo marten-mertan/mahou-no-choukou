fb899845b254311fea6fca3fe835534310831074
marten-mertan <mertangp@bk.ru>
2022-08-02 17:13:03 +0300

    edit static paths

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -43,12 +43,12 @@ export default defineNuxtConfig({
             ],
             link: [
                 /* Favicons */
-                {rel: 'icon', type: 'image/x-icon', href: '/static/favicons/favicon.ico'},
-                {rel: 'icon', type: 'image/png', sizes: '32x32', href: '/static/favicons/favicon-32x32.png'},
-                {rel: 'icon', type: 'image/png', sizes: '16x16', href: '/static/favicons/favicon-16x16.png'},
-                {rel: 'apple-touch-icon', sizes: '180x180', href: '/static/favicons/apple-touch-icon.png'},
-                {rel: 'manifest', href: '/static/favicons/site.webmanifest'},
-                {rel: 'mask-icon', href: '/static/favicons/safari-pinned-tab.svg', color: '#ffd8d8'},
+                {rel: 'icon', type: 'image/x-icon', href: 'favicons/favicon.ico'},
+                {rel: 'icon', type: 'image/png', sizes: '32x32', href: 'favicons/favicon-32x32.png'},
+                {rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicons/favicon-16x16.png'},
+                {rel: 'apple-touch-icon', sizes: '180x180', href: 'favicons/apple-touch-icon.png'},
+                {rel: 'manifest', href: 'favicons/site.webmanifest'},
+                {rel: 'mask-icon', href: 'favicons/safari-pinned-tab.svg', color: '#ffd8d8'},
             ],
         },
 
